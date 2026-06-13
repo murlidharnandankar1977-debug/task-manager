227044e0a153879568fba3529419590385791d5e
shreya <murlidharnandankar1977@gmail.com>
2026-06-13 20:55:28 +0530

    Fix production API URL

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -12,7 +12,7 @@ function Login() {
   const handleLogin = async () => {
     try {
       const response = await axios.post(
-        "http://localhost:5000/api/auth/login",
+        "https://task-manager-89pc.onrender.com/api/auth/login",
         {
           email,
           password,
