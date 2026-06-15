662ec7d4f4f5cdd81265c66c7520174de56c1a0b
shreya <murlidharnandankar1977@gmail.com>
2026-06-15 19:18:17 +0530

    Fix CORS

--- server/server.js
+++ server/server.js
@@ -11,7 +11,13 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/tasks", taskRoutes);
