acbef144c156148c09763b18e4a262a80a40c227
shreya <murlidharnandankar1977@gmail.com>
2026-06-15 18:38:33 +0530

    Fix production API URLs

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -8,7 +8,7 @@ function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const API_URL = "https://task-manager-zhnr.onrender.com";
+  const API_URL = "https://task-manager-zhnr.onrender.com/api/tasks";
 
   const handleLogin = async () => {
     try {
